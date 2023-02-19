69663ba7fac5c3751150c3c5ece20be9c04789ce
envoyr <hello@envoyr.com>
2023-02-19 22:22:13 +0100

    add header
    Signed-off-by: envoyr <hello@envoyr.com>

---
+++ src/runtime/components/BitinflowHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-between items-center">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BitinflowHeader",
+}
+</script>
---
+++ src/runtime/components/BitinflowHeaderTitle.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-2xl dark:text-white">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BitinflowHeaderTitle",
+}
+</script>
